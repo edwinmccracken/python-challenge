33f3ddac7a08e0ce0c6e4a054c3f3db9ee1acb03
eddiemccracken <edwinmccracken@gmail.com>
2020-02-19 20:14:14 -0800

    third commit PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,3 +8,27 @@ with open(budget_data_csv, 'r') as csvfile:
     next(csvreader)
     for row in csvreader:
         print(row)
+
+
+file = open(budget_data_csv, 'r')
+numline_months = len(file.readlines())
+numline_months = numline_months - 1
+print(numline_months)
+
+with open (budget_data_csv, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    header = None
+    for row in csvreader:
+        if header is None:
+            header = row
+            mySums=[0]*len(row)
+            continue
+        else:
+            for i,x in enumerate(row):
+                try:
+                    converted=float(x)
+                except ValueError:
+                    converted = 0
+                mySums[i]+=converted
+
+                print(mySums)
