672be826b5d93364dc31e2b8ce9ccc68aa1678df
eddiemccracken <edwinmccracken@gmail.com>
2020-02-20 21:52:07 -0800

    final_commit_of_PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,34 +1,42 @@
+#Import modules
 import os
 import csv
 
+#Connect the file path
 budget_data_csv = os.path.join('budget_data.csv')
 
+#Set up open statement and loop
 with open(budget_data_csv, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     next(csvreader)
+    revenue = []
+    date = []
+    rev_change = []
+
     for row in csvreader:
-        print(row)
 
+        revenue.append(float(row[1]))
+        date.append(row[0])
 
-file = open(budget_data_csv, 'r')
-numline_months = len(file.readlines())
-numline_months = numline_months - 1
-print(numline_months)
+#Set the titles of the analysis
+    print("Financial Analysis")
+    print("Total Months:", len(date))
+    print("Total Revenue: $", sum(revenue))
 
-with open (budget_data_csv, 'r') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',')
-    header = None
-    for row in csvreader:
-        if header is None:
-            header = row
-            mySums=[0]*len(row)
-            continue
-        else:
-            for i,x in enumerate(row):
-                try:
-                    converted=float(x)
-                except ValueError:
-                    converted = 0
-                mySums[i]+=converted
+ #Figure out the revenue change, min and max values and dates of min and max change
+    for i in range(1,len(revenue)):
+        rev_change.append(revenue[i] - revenue[i-1])   
+        avg_rev_change = sum(rev_change)/len(rev_change)
 
-                print(mySums)
+        max_rev_change = max(rev_change)
+
+        min_rev_change = min(rev_change)
+
+        max_rev_change_date = str(date[rev_change.index(max(rev_change))])
+        min_rev_change_date = str(date[rev_change.index(min(rev_change))])
+
+#Print results of 
+    print("Avereage Revenue Change: $", round(avg_rev_change))
+    print("Greatest Increase in Revenue:", max_rev_change_date,"($", max_rev_change,")")
+    print("Greatest Decrease in Revenue:", min_rev_change_date,"($", min_rev_change,")")
+
